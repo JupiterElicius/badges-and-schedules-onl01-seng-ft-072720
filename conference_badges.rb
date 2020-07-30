e36b3923ee2112ca754b8221c7fdce934175f2b2
Nick Bazaldua <bazalnick@gmail.com>
2020-07-30 04:09:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,10 +14,10 @@ def assign_rooms(attendees)
   attendees.each_with_index do |attendee, i|
  new_array.push ("Hello, #{attendee}! You'll be assigned to room #{i = i + 1}!")
  end
-end 
  return new_array
 
 def printer(attendees)
+  
   batch_badge_creator(attendees).each do |person|
         puts person
   end    
