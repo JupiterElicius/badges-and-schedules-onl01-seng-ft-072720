357806eeea79160403ce89dac1ea1fcf2b157930
Nick Bazaldua <bazalnick@gmail.com>
2020-07-30 04:14:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,8 @@ def assign_rooms(attendees)
   counter = 0
   attendees.each_with_index do |attendee, i|
  new_array.push ("Hello, #{attendee}! You'll be assigned to room #{i = i + 1}!")
-  end
-  =
+
+
  return new_array
 
 def printer(attendees)
